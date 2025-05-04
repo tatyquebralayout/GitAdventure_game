fa12f124b8620b8107f260588d69b7ca0346b841
Tatiana Barros <coluninja@gmail.com>
2025-05-03 21:53:50 -0300

    Remove vite-plugin-swc from dependencies and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import swc from 'vite-plugin-swc'
+// import swc from 'vite-plugin-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    swc({
-      swcOptions: {
-        jsc: {
-          parser: {
-            syntax: "typescript"
-          }
-        }
-      }
-    })
+    react()
   ],
   esbuild: false,            // desabilita esbuild se for usar SWC 100%
 })
